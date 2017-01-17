69bcb7b8a935d8e1c675d82219c4393808fb0167
liuyun <atme_talk@163.com>
2017-01-17 14:59:30 +0800

    bug fixed

--- src/views/find.vue
+++ src/views/find.vue
@@ -1,11 +1,86 @@
 <template>
-  <div class="find">iam find</div>
+  <div class="find">
+    <div class="header-wrapper">
+      <vheader
+        :isNav="false"
+        :basic="false"
+        basic-str="TOP 100"></vheader>      
+    </div>
+
+    <div class="find-content">
+      <div class="find-panel">
+        <div class="card-wrapper" v-for="n in 100">
+          <card></card>
+        </div> 
+      </div>   
+    </div>
+
+    <div class="find-random">
+      <div class="random-btn" @click="random">随机查看</div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {}
+  import vheader from 'components/header'
+  import card from 'components/card'
+
+  export default {
+    methods: {
+      random () {
+        const clientWidth = document.documentElement.clientWidth || document.body.clientWidth // 视口宽度
+        const cardWidth = clientWidth * 0.47 // 卡片组件的宽度
+        const cardHeight = 220 + clientWidth * 0.06 // 卡片组件的高度
+        const horizontalDis = -(Math.floor(Math.random() * 9) * cardWidth) + 'px' // 随机移动的水平距离
+        const varticalDis = -(Math.floor(Math.random() * 9) * cardHeight) + 'px' // // 随机移动的垂直距离
+        const panel = document.querySelector('.find-panel')
+        panel.style.transform = `translate(${horizontalDis}, ${varticalDis})`
+      }
+    },
+    components: {
+      vheader,
+      card
+    }
+  }
 </script>
 
 <style lang='stylus'>
-
+  .find
+    .header-wrapper
+      position: fixed
+      top: 0
+      left: 0
+      width: 100%
+      z-index: 1000
+    .find-content
+      width: 100vw
+      height: 100vh
+      margin-top: 55px
+      overflow: hidden
+      .find-panel
+        width: calc(100vw * 5)
+        font-size: 0
+        transition: all 1s cubic-bezier(.5,.5,.5,1.08)
+        .card-wrapper
+          display: inline-block
+          margin: 6vw 0 0 6vw
+    .find-random
+      position: fixed
+      bottom: 80px
+      width: 100%
+      height: 30px
+      text-align: center
+      z-index: 100
+      .random-btn
+        width: 100px
+        height: 30px
+        line-height: 30px
+        margin: 0 auto
+        border: 1px solid #1e8ae8
+        border-radius: 2px
+        font-size: 16px
+        color: #000
+        font-weight: 700
+        color: #1e8ae8
+      
 </style>
